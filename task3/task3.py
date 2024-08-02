aa43f1d727e81b12f7c79c1f1c88f1df3bd9daf0
Evgeny Rabichev <31814504+coolspace@users.noreply.github.com>
2024-08-02 17:14:33 +0300

    Update task3.py

--- task3/task3.py
+++ task3/task3.py
@@ -1,7 +1,11 @@
 import json
-with open('values.json', 'r') as values_file:
+
+values = input("Введите путь первого файла со значениями: ")
+tests = input("Введите путь второго файла с тестами: ")
+report = input("Введите путь второго файла с отчетом: ")
+with open(values, 'r') as values_file:
     values_data=json.load(values_file)
-with open('tests.json', 'r') as tests_file:
+with open(tests, 'r') as tests_file:
     tests_data=json.load(tests_file)
 
 
@@ -66,5 +70,6 @@ for test_item in tests_data['tests'][2]['values'][1]['values'][0]['values']:
         if values_item['id']==test_id:
             test_item['value']=values_item['value']
             break
-with open('report.json', 'w') as report_file:
+with open(report, 'w') as report_file:
     json.dump(tests_data, report_file, indent=4)
+
